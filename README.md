e9f27d8973331706f7e3e724a544b0abb6a2f1bc
Jonas Galvez <jonasgalvez@gmail.com>
2020-08-15 13:51:45 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Nuxt ESBuild module
+
+Enables [`esbuild-loader`](https://github.com/egoist/esbuild-loader) to transpile JavaScript and TypeScript in Nuxt.
+
+## Install
+
+```sh
+npm i nuxt-esbuild --save
+```
+
+In `nuxt.config.js`:
+
+```js
+modules: [
+  'nuxt-esbuild'
+]
+```
+
+## Notice
+
+This is entirely experimental, no guarantees it will work for everyone.
+
+Follow the [esbuild project](https://github.com/evanw/esbuild) for latest info on its reliability and options.
---
+++ index.js
@@ -0,0 +1,25 @@
+const { ESBuildPlugin } from 'esbuild-loader'
+
+const kRegistered = Symbol.for('registered')
+
+module.exports = function () {
+  this.extendBuild((config) => {
+    const jsxRuleIndex = config.module.rules.findIndex(r => '.jsx'.match(r.test))
+    if (config.module.rules[jsxRuleIndex][kRegistered]) {
+      return
+    }
+    config.module.plugins.push(new ESBuildPlugin())
+    config.module.rules.splice(jsxRuleIndex, 1, {
+      test: config.module.rules[jsxRuleIndex].test,
+      use: [
+        {
+          loader: 'esbuild-loader',
+          options: {
+            target: 'es2015',
+          }
+        }
+      ]
+    })
+    config.module.rules[jsxRuleIndex][kRegistered] = true
+  })
+}
