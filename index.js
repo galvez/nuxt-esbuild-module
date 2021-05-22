1fac7e2afffae0a2eda696643545b52653ed5dac
Jonas Galvez <jonasgalvez@gmail.com>
2021-05-22 15:52:42 -0300

    bump: 1.1.0, ESBuildPlugin no longer required

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const { ESBuildPlugin } = require('esbuild-loader')
-
 const { assign } = Object
+
 const kRegistered = Symbol.for('registered')
 
 const defaultOptions = {
@@ -16,7 +15,6 @@ module.exports = function (moduleOptions = {}) {
     }
     const originalUse = config.module.rules[jsxRuleIndex].use
     if (this.options.dev || options.loader === 'ts') {
-      config.plugins.push(new ESBuildPlugin())
       config.module.rules.splice(jsxRuleIndex, 1, {
         test: /\.((m?jsx?)|(ts))$/,
         use: [
